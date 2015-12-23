1d6fa57d8be2216350e2360ca1c3af6779646d33
Jack Burns <jack@raptiq.com>
2015-12-23 10:22:48 -0500

    initial project setup, getting going

---
+++ app/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+	render() {
+		return (
+				<div className="alert alert-info">
+					Welp stuff is working i guess
+				</div>	
+			)
+	}
+}
+
+export default Home;
